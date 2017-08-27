70764d38b0d459c47d7cee257ccf5c7af962c85c
jackza001 <31381937+jackza001@users.noreply.github.com>
2017-08-27 16:48:13 +0700

    2-38

---
+++ 2-38.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("%1s\n","58224090101");
+	printf("%1s\n","Wiphart Nakthong");
+	printf("%1s\n\n\n\n\n\n","16/02/28");
+	printf("%120s\n","http://www.npru.ac.th");
+	printf("%57s%s","","Bye bye");
+	return 0;
+}
